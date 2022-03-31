9d600b84bc9dd065d27237162577ac294b0b34d9
thelostword <thelostword@163.com>
2022-03-31 12:49:37 +0800

    feat: push

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: losting
+ * @Date: 2022-03-31 12:20:41
+ * @LastEditTime: 2022-03-31 12:48:52
+ * @LastEditors: losting
+ * @Description: 
+ * @FilePath: \github-actions-demo\src\App.vue
+-->
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
@@ -6,7 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <HelloWorld msg="PUSH" />
 </template>
 
 <style>
