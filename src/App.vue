0a29d63b27b6d4e61974c86626f9310293d3a04e
thelostword <thelostword@163.com>
2022-03-31 14:58:50 +0800

    ci: modified ci

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: losting
  * @Date: 2022-03-31 12:20:41
- * @LastEditTime: 2022-03-31 12:48:52
+ * @LastEditTime: 2022-03-31 14:33:56
  * @LastEditors: losting
  * @Description: 
  * @FilePath: \github-actions-demo\src\App.vue
@@ -10,11 +10,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+const env = import.meta.env.DEV
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="PUSH" />
+  {{env}}-222222222222222222
 </template>
 
 <style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+/*
+ * @Author: losting
+ * @Date: 2022-03-31 12:20:41
+ * @LastEditTime: 2022-03-31 14:50:15
+ * @LastEditors: losting
+ * @Description: 
+ * @FilePath: \github-actions-demo\vite.config.js
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default ({mode}) => {
+  const __DEV__ = mode === 'development'
+  return defineConfig({
+    base: __DEV__ ? './' : './github-actions-demo/',
+    plugins: [vue()]
+  })
+}
