f40a20143911694097d8ca51a1cc0b5191fd0c0c
thelostword <thelostword@163.com>
2022-03-31 15:54:43 +0800

    build: .env change

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
 /*
  * @Author: losting
  * @Date: 2022-03-31 12:20:41
- * @LastEditTime: 2022-03-31 14:50:15
+ * @LastEditTime: 2022-03-31 15:46:22
  * @LastEditors: losting
  * @Description: 
  * @FilePath: \github-actions-demo\vite.config.js
  */
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// https://vitejs.dev/config/
-export default ({mode}) => {
-  const __DEV__ = mode === 'development'
+export default ({ mode }) => {
   return defineConfig({
-    base: __DEV__ ? './' : './github-actions-demo/',
-    plugins: [vue()]
+    base: loadEnv(mode, process.cwd()).VITE_BASE_PATH,
+    plugins: [vue()],
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: "static/js/[name]-[hash].js",
+          entryFileNames: "static/js/[name]-[hash].js",
+          assetFileNames: "static/[ext]/[name]-[hash].[ext]",
+        },
+      },
+    },
   })
 }
